722b6a0b1259c9fc6d906e860aa0465b374a8b36
Alexander <alexander.ivanov.289@gmail.com>
2020-07-25 20:51:15 +0300

    Changed port. Add procfile

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Application have been started.');
 });
 
